36cac95eee4d5d3ada6fcf8b06bf8b84115fe86c
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-06-09 13:51:13 +0100

    Add toolbar and Navigation bar to the meme generator page

---
+++ MemeMe/meme.swift
@@ -0,0 +1,17 @@
+//
+//  meme.swift
+//  MemeMe
+//
+//  Created by Rasheed Sulayman on 09/06/2018.
+//  Copyright © 2018 r4sh33d. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var buttomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
