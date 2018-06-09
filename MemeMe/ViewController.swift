6e0e1493bb0748e5afea6cd216a037b63d275613
r4sh33d <1>
2018-06-09 04:37:52 +0100

    Add TextFields to the meme generator page

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate , UITextFieldDelegate {
     @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var buttomTextField: UITextField!
     
  
 
@@ -20,7 +24,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buttomTextField.textAlignment = .center
+        topTextField.textAlignment = .center
+        topTextField.delegate = self
+        buttomTextField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -38,11 +58,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func pickImageFromAlbum(_ sender: Any) {
         let controller = UIImagePickerController()
         controller.delegate = self
+        controller.sourceType = .photoLibrary
         present(controller, animated: true, completion: nil)
     }
     
     @IBAction func pickImageFromCamera(_ sender: Any) {
-        
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .camera
+        present(controller, animated: true, completion: nil)
     }
     
     
